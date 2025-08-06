cf89a7baadc4b1b6fc0a57c2087cb638b69b3721
Krishna Yadav <yadavkrish2709@gmail.com>
2025-08-06 21:04:17 +0530

    added features like display of tasks added in a list style

--- src/app/page.js
+++ src/app/page.js
@@ -4,14 +4,29 @@ import React, { useState } from "react";
 const page = () => {
   const [title, settitle] = useState("");
   const [desc, setdesc] = useState("");
+  const [mainTask, setMainTask] = useState([]);
 
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log(title);
-    console.log(desc);
+    setMainTask([...mainTask, { title, desc }]);
     settitle("");
     setdesc("");
+    console.log(mainTask);
   };
+
+  let renderTask = <h2>No Task available</h2>;
+  if (mainTask.length > 0) {
+    renderTask = mainTask.map((t, i) => {
+      return (
+        <li>
+          <div className="flex justify-between mb-5">
+            <h5 className="text-2xl font-semibold">{t.title}</h5>
+            <h6 className="text-xl font-semibold">{t.desc}</h6>
+          </div>
+        </li>
+      );
+    });
+  }
   return (
     <>
       <h1 className="bg-black text-white text-center text-3xl font-bold py-2">
@@ -36,10 +51,13 @@ const page = () => {
             setdesc(e.target.value);
           }}
         />
-        <button className="text-white bg-black rounded font-semibold border-2 border-zinc-600 px-2 mx-4">
+        <button className="text-white bg-black rounded font-semibold border-2 border-zinc-600 px-2 mx-4 my-2">
           Add task
         </button>
       </form>
+      <div className="p-8 bg-slate-200 mt-2">
+        <ul>{renderTask}</ul>
+      </div>
     </>
   );
 };
